9d1b07cb47bc99293c3bbddf5f4e9d94ec3122c8
develephant <cbyerley@develephant.com>
2022-09-22 14:33:15 -0500

    update

--- filesystem/fs.lua
+++ filesystem/fs.lua
@@ -13,12 +13,60 @@ _M.DOCUMENTS_DIR = system.DocumentsDirectory
 _M.RESOURCES_DIR = system.ResourceDirectory
 _M.TEMP_DIR = system.TemporaryDirectory
 
+--alias
+_M.sep = _M.getPathSeparator
+
 function _M.new( options )
   options = options or {}
-
   options.defaultDir = options.default_dir or system.DocumentsDirectory
-
   return setmetatable( options, mt)
+end
+
+--##############################################################################
+-- Paths
+--##############################################################################
+function _M.split(self, inputstr, sep_token)
+  if sep_token == nil then
+    sep_token = "%s"
+  end
+  local t = {}
+  local i = 1
+  for str in string.gmatch(inputstr, "([^" .. sep_token .. "]+)") do
+    t[i] = str
+    i = i + 1
+  end
+  return t
+end
+
+function _M.trim(self, str)
+  return (string.gsub(str, "^%s*(.-)%s*$", "%1"))
+end
+
+function _M.normalize(self, path, sep)
+  sep = sep or self.sep()
+  return (string.gsub(path, "%"..sep.."+", sep))
+end
+
+function _M.join( self, ... )
+  local paths = { ... }
+  local sep = self.sep()
+  return _M.normalize(table.concat( paths, sep ))
+end
+
+function _M.basename( self, path )
+  path = _M.normalize(path)
+  local parts = _M.split(path, self.sep())
+  return table.remove(parts)
+end
+
+--file extension
+function _M.extension(self, file)
+  return self.split(self, file, '.')[2]
+end
+
+--filename sans extension
+function _M.filename(self, file)
+  return self.split(self, file, '.')[1]
 end
 
 function _M.getPath(self, path, dir)
@@ -35,6 +83,58 @@ function _M.fileExists(self, path)
   return true
 end
 
+function _M.isDir( self, path )
+  local res, err = lfs.attributes(path, "mode")
+  if not res then
+    return nil, err
+  end
+  if res == "directory" then
+    return true
+  end
+  return nil, 'directory not found.'
+end
+
+function _M.isFile( self, path )
+  local res, err = lfs.attributes(path, "mode")
+  if not res then
+    return nil, err
+  end
+  if res == "file" then
+    return true
+  end
+  return nil, 'file not found.'
+end
+
+--list directory
+function _M.listDir(self, dirpath)
+  local list = {}
+  for file in lfs.dir(dirpath) do
+    if file ~= "." and file ~= ".." then
+      if not fs.isDir(dirpath.."/"..file) then
+        table.insert(list, file)
+      end
+    end
+  end
+  return list
+end
+
+--is empty directory
+function _M.isDirEmpty(self, dirpath)
+  local list = {}
+  for file in lfs.dir(dirpath) do
+    if file ~= "." and file ~= ".." then
+      table.insert(list, file)
+    end
+  end
+  if #list == 0 then
+    return true
+  end
+  return false
+end
+
+--##############################################################################
+-- Files
+--##############################################################################
 function _M.loadFile(self, path)
   local fd, err = io.open(self:getPath(path), 'r')
   if not fd then
@@ -107,6 +207,54 @@ function _M.makeDocumentsDir(self, dir_name)
     return lfs.currentdir() .. "/" .. dir_name
   else
     return nil, "could not create directory."
+  end
+end
+
+--##############################################################################
+-- Device
+--##############################################################################
+function _M.getEnvironment()
+  return system.getInfo("environment")
+end
+
+function _M.isSim()
+  if _M.getEnvironment() == "simulator" then
+    return true
+  end
+
+  return false
+end
+
+function _M.getPlatform()
+  local platform = system.getInfo("platform")
+  if platform == _M.WIN32 then
+    return _M.WIN32
+  elseif platform  == _M.MACOS then
+    return _M.MACOS
+  else
+    return false
+  end
+end
+
+function _M.isMACOS()
+  if _M.getPlatform() == _M.MACOS then
+    return true
+  end
+end
+
+function _M.isWIN32()
+  if _M.getPlatform() == _M.WIN32 then
+    return true
+  end
+end
+
+function _M.getPathSeparator()
+  if _M.isMACOS() then
+    return [[/]]
+  elseif _M.isWIN32() then
+    return [[\]]
+  else
+    return [[/]]
   end
 end
 
--- main.lua
+++ main.lua
@@ -1,25 +1,27 @@
 -- REQUIRES Alignmate (https://github.com/develephant/solar2d-alignmate-lib)
 
-local Text = require('ui.text')
-local Button = require('ui.button')
-local Alignmate = require('alignment.alignmate')
+-- local Text = require('ui.text')
+-- local Button = require('ui.button')
+-- local Alignmate = require('alignment.alignmate')
 
-local TitleText = Text.new({ text = "Jello", fontSize = 48 })
-local HelloText = Text.new({ text = "Hello", fontSize = 48 })
-local MellowText = Text.new({ text = "Mellow", fontSize = 48 })
+-- local TitleText = Text.new({ text = "Jello", fontSize = 48 })
+-- local HelloText = Text.new({ text = "Hello", fontSize = 48 })
+-- local MellowText = Text.new({ text = "Mellow", fontSize = 48 })
 
-Alignmate.center(TitleText)
-Alignmate.centerRight(HelloText)
-Alignmate.centerLeft(MellowText)
+-- Alignmate.center(TitleText)
+-- Alignmate.centerRight(HelloText)
+-- Alignmate.centerLeft(MellowText)
 
-local function onSubmit(e)
-    print('clicked')
-    TitleText.text = "Swello"
-end
+-- local function onSubmit(e)
+--     print('clicked')
+--     TitleText.text = "Swello"
+-- end
 
-local submitButton = Button.new({ 
-    label = "Submit",
-    onPress = onSubmit 
-})
+-- local submitButton = Button.new({ 
+--     label = "Submit",
+--     onPress = onSubmit 
+-- })
 
-Alignmate.bottom(submitButton, { padBottom = 20 })
+-- Alignmate.bottom(submitButton, { padBottom = 20 })
+
+local fs = require("filesystem.fs").new()
